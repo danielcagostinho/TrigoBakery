d4634268a54da5da3cba7a46ab804a9c2173c30c
danielcagostinho <danielcagostinho@gmail.com>
2021-01-14 10:18:36 -0500

    Fixed Most of the Offset Margins. Small tweaks

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -23,6 +23,8 @@ const LandingPage = () => {
   return (
     <div>
       <div className="content">
+        <Header />
+        <HeaderImage />
         <div className="section color" id="bread">
           <div className="row">
             <div className="column">
@@ -35,7 +37,9 @@ const LandingPage = () => {
                 </p>
               </div>
             </div>
-            <div className="column"></div>
+            <div className="column">
+              {/* <img src={ingredientsImage} alt="Various Baker's Ingredients" /> */}
+            </div>
           </div>
         </div>
         <div className="section white">
@@ -50,8 +54,7 @@ const LandingPage = () => {
               <h3>Made fresh every morning.</h3>
               <p>
                 Bread may be leavened by naturally occurring microbes,
-                chemicals, industrially produced yeast, or high-pressure
-                aeration. In many countries
+                chemicals, industrially produced yeast
               </p>
             </div>
           </div>
@@ -99,6 +102,33 @@ const LandingPage = () => {
             </div>
           </div>
         </div>
+        <div className="section white">
+          <div className="row">
+            <div id="insta">
+              <h2>Check out</h2>
+              <h2 id="insta-link">
+                Trigo on{" "}
+                <a
+                  href="https://www.instagram.com/trigobakeryinc/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  instagram...
+                </a>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="section white">
+          <div className="row">
+            <div id="carousel">
+              <img src={carousel1} alt="pastries" />
+              <img src={carousel2} alt="pastries" />
+              <img src={carousel3} alt="pastries" />
+              <img src={carousel4} alt="pastries" />
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* <Header />
@@ -117,111 +147,9 @@ const LandingPage = () => {
             </p>
           </div>
           <div className="column right" id="ingredients">
-            <img src={ingredientsImage} alt="Various Baker's Ingredients" />
+            
           </div>
         </div>
-      </div>
-      <div className="landing-section white">
-        <div className="content">
-          <div className="column">
-            <div className="fresh-images">
-              <img
-                className="fresh-img"
-                alt="baker working"
-                src={landingImage1}
-              />
-              <img
-                className="fresh-img"
-                alt="baker working"
-                src={landingImage2}
-              />
-            </div>
-          </div>
-          <div className="column" id="fresh-images-text">
-            <h2>Made fresh every morning.</h2>
-            <p>
-              Bread may be leavened by naturally occurring microbes, chemicals,
-              industrially produced yeast, or high-pressure aeration. In many
-              countries, commercial bread often contains additives to improve
-              flavor
-            </p>
-          </div>
-        </div>
-      </div>
-      
-      <div className="landing-section color">
-        <div className="content" id="map-row">
-          <div className="column address">
-              <p className="address-title">Where to find us</p>
-              <h2>289 Dundas St E. Mississauga, ON L5A 1X1</h2>
-              <hr />
-              <p>
-                or call us at <span>(905) 896-7245</span>
-              </p>
-          </div>
-          <div className="column">
-            <div className="map">
-              <iframe
-                title="Trigo on Google Maps"
-                width={mapWidth}
-                height={mapHeight}
-                frameBorder="0"
-                // style="border:0"
-                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJncRCmQFHK4gRGzsBpoT5mW8&key=AIzaSyC0iarw2tL_5dhFdhdMb87BO97VkxePfe8"
-                allowFullScreen
-              ></iframe>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="landing-section white">
-        <div className="content" id="hours-row">
-          <div className="column hours-img-container">
-            <div className="left">
-              <img
-                className="hours-img"
-                alt="Outdoor Table"
-                src={trigoOutside}
-              />
-            </div>
-          </div>
-          <div className="column hours-of-operation">
-            <div className="right">
-              <h2>We're up <br/> bright and early.</h2>
-              <p className="hours">6:00AM - 7:00PM</p>
-              <p className="days">Monday - Friday</p>
-              <p className="hours">6:00AM - 5:00PM</p>
-              <p className="days">Saturday - Sunday</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div className="landing-section white">
-        <div className="insta">
-          <h2>Check out</h2>
-          <h2 className="insta-link">
-            Trigo on{" "}
-            <a
-              href="https://www.instagram.com/trigobakeryinc/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              instagram...
-            </a>
-          </h2>
-        </div>
-        <div></div>
-      </div>
-
-      <div className="landing-section white">
-        <div className="carousel">
-          <img src={carousel1} alt="pastries" />
-          <img src={carousel2} alt="pastries" />
-          <img src={carousel3} alt="pastries" />
-          <img src={carousel4} alt="pastries" />
-        </div>
-        <div></div>
       </div>
 
       <div className="landing-section white">
