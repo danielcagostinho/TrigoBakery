b6b1f6dd294aa14cc8f1025e35e6636faa51292f
danielcagostinho <danielcagostinho@gmail.com>
2021-02-03 22:10:36 -0500

    Changed Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./containers/LandingPage/LandingPage";
 import MenuPage from "./containers/MenuPage/MenuPage";
@@ -304,7 +304,7 @@ const cakes = [
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/">
         <ScrollToTop>
           <NavBar />
           <Switch>
